d380f2ac7e5038e2c217bc21d1f7773c69fbd432
Leeten <lchin@imsystems.com.au>
2020-11-02 08:51:22 +1100

    v1.1

--- create.js
+++ create.js
@@ -2,6 +2,17 @@ import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
+//functions to return current date time of the caller in YYYY-MM-DD HH24:MI:SS format
+function getDateStr() {
+    const curdate = new Date();
+    return curdate.getFullYear() + "-" +
+        ("00" + (curdate.getMonth() + 1)).slice(-2) + "-" +
+        ("00" + curdate.getDate()).slice(-2) + " " +
+        ("00" + curdate.getHours()).slice(-2) + ":" +
+        ("00" + curdate.getMinutes()).slice(-2) + ":" +
+        ("00" + curdate.getSeconds()).slice(-2);
+}
+
 export const main = handler(async (event, context) => {
     const data = JSON.parse(event.body);
     const params = {
@@ -13,13 +24,13 @@ export const main = handler(async (event, context) => {
         // - 'noteId': a unique uuid
         // - 'content': parsed from request body
         // - 'attachment': parsed from request body
-        // - 'createdAt': current Unix timestamp
+        // - 'createdAt': current timestamp
         Item: {
             userId: event.requestContext.identity.cognitoIdentityId,
             noteId: uuid.v1(),
             content: data.content,
             attachment: data.attachment,
-            createdAt: Date.now()
+            createdAt: getDateStr()
         }
     };
 
